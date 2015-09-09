af4ef2290226bf556ad36269db41c80d61081ac3
Oreshko Slava <ardlion108@gmail.com>
2015-09-09 15:19:09 +0300

    add model

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class Persons(models.Model):
+    name = models.CharField(max_length=21, unique=True)
+    last_name = models.CharField(max_length=21)
+    date_birth = models.DateField()
+    bio = models.TextField(verbose_name='biography')
+
+    def __str__(self):
+        return "{0} {1}".format(self.name, self.last_name)
+
+
+class PersonsContacts(models.Model):
+    name = models.ForeignKey(Persons)
+    email = models.EmailField(unique=True, verbose_name='email')
+    jabber_JID = models.EmailField(unique=True)
+    skype = models.CharField(max_length=21)
+    twitter = models.CharField(max_length=21)
+    tel = models.CharField(max_length=21)
