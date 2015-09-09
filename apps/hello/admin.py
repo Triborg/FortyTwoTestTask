b0dcc23e79c2c04c122cc1676feef301772309d2
Oreshko Slava <ardlion108@gmail.com>
2015-09-09 15:18:45 +0300

    add admin

--- apps/hello/admin.py
+++ apps/hello/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from apps.hello.models import Persons, PersonsContacts
+admin.autodiscover()
 
-# Register your models here.
+
+class PersonsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'last_name', 'date_birth', 'bio')
+
+
+class PersonsContactsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'jabber_JID', 'skype', 'tel')
+
+
+admin.site.register(Persons, PersonsAdmin)
+admin.site.register(PersonsContacts, PersonsContactsAdmin)
